6b1b7d2d9b04763e914618042cb4d902ea4c4fe8
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-08-20 22:07:52 +0300

    commit tree 4ed91b941fa52d5ee2f819c23285e99c3d93ea94

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,38 @@
 class Person
   # your code here
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", attributes[key])
+    end
+  end
 end
+
+lynn_attributes = {
+  name: "Lynn",
+  birthday: "01/29/1984",
+  hair_color: "brown",
+  eye_color: "brown",
+  height: "5'11",
+  weight: "160 lbs",
+  handed: "right",
+  complexion: "light",
+  t_shirt_size: "large",
+  wrist_size: "medium",
+  glove_size: "medium",
+  pant_length: "medium",
+  pant_width: "medium"
+}
+
+lynn = Person.new(lynn_attributes)
+
+dahmer_attributes = {
+  name: "Dahmer",
+  birthday: "01/29/1984",
+  hair_color: "brown",
+  eye_color: "brown",
+  height: "5'11",
+  weight: "160 lbs"
+}
+
+dahmer = Person.new(dahmer_attributes)
